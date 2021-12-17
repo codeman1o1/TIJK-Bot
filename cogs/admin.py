9dcda0e5d35390df3b8f6c051e93974ebe1a76a1
codeman1o1 <karsten.herijgers@gmail.com>
2021-12-17 22:53:38 +0100

    Fixed a bug where the buttons stopped working after a bot restart

--- cogs/admin.py
+++ cogs/admin.py
@@ -26,9 +26,6 @@ class admin(
 ):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
         self.bot.add_view(RoleView())
 
     @commands.command(
