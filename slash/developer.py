3911e4a6597de3d3e0f6833e0c4dd169630ec014
codeman1o1 <karsten.herijgers@gmail.com>
2022-03-23 18:05:26 +0100

    Added slash checks

--- slash/developer.py
+++ slash/developer.py
@@ -1,7 +1,8 @@
 import nextcord
 from nextcord.ext import commands
-from nextcord import Interaction
+from nextcord import Interaction, slash_command
 from nextcord.application_command import SlashOption
+import nextcord.ext.application_checks as checks
 import os, sys
 
 from main import logger
@@ -15,11 +16,12 @@ class developer_slash(
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @nextcord.slash_command(
+    @slash_command(
         name="embed",
         description="Generate an embed",
         guild_ids=[870973430114181141, 865146077236822017],
     )
+    @checks.has_any_role("Owner", "Admin", "TIJK-Bot developer")
     async def embed(
         self,
         interaction: Interaction,
@@ -51,79 +53,64 @@ class developer_slash(
             name="value3", description="The value of the 3d field", required=False
         ),
     ):
-        owner = nextcord.utils.get(interaction.guild.roles, name="Owner")
-        admin = nextcord.utils.get(interaction.guild.roles, name="Admin")
-        tijk_bot_developer = nextcord.utils.get(
-            interaction.guild.roles, name="TIJK-Bot developer"
-        )
-        admins = (owner, admin, tijk_bot_developer)
-        if any(role in interaction.user.roles for role in admins):
-            try:
-                if not color:
-                    color = 0x0DD91A
-                if title:
-                    embed = nextcord.Embed(color=color, title=title)
-                else:
-                    embed = nextcord.Embed(color=color)
-                if name1 and value1:
-                    embed.add_field(
-                        name=name1,
-                        value=value1,
-                        inline=False,
-                    )
-                if name2 and value2:
-                    embed.add_field(
-                        name=name2,
-                        value=value2,
-                        inline=False,
-                    )
-                if name3 and value3:
-                    embed.add_field(
-                        name=name3,
-                        value=value3,
-                        inline=False,
-                    )
-                await interaction.response.send_message(embed=embed)
-            except nextcord.errors.HTTPException:
-                await interaction.response.send_message(
-                    "The embed is invalid", ephemeral=True
+        try:
+            if not color:
+                color = 0x0DD91A
+            if title:
+                embed = nextcord.Embed(color=color, title=title)
+            else:
+                embed = nextcord.Embed(color=color)
+            if name1 and value1:
+                embed.add_field(
+                    name=name1,
+                    value=value1,
+                    inline=False,
                 )
-        else:
+            if name2 and value2:
+                embed.add_field(
+                    name=name2,
+                    value=value2,
+                    inline=False,
+                )
+            if name3 and value3:
+                embed.add_field(
+                    name=name3,
+                    value=value3,
+                    inline=False,
+                )
+            await interaction.response.send_message(embed=embed)
+        except nextcord.errors.HTTPException:
             await interaction.response.send_message(
-                "You do not have permission to perform this task", ephemeral=True
+                "The embed is invalid", ephemeral=True
             )
 
-    @nextcord.slash_command(
+    @slash_command(
         name="restart",
         description="Restarts TIJK Bot",
         guild_ids=[870973430114181141, 865146077236822017],
     )
+    @checks.is_owner()
     async def restart(self, interaction: nextcord.Interaction):
-        if await self.bot.is_owner(interaction.user):
-            embed = nextcord.Embed(color=0x0DD91A)
-            embed.add_field(
-                name="TIJK Bot is restarting...",
-                value=f"TIJK Bot was restarted by {interaction.user}",
-                inline=False,
+        embed = nextcord.Embed(color=0x0DD91A)
+        embed.add_field(
+            name="TIJK Bot is restarting...",
+            value=f"TIJK Bot was restarted by {interaction.user}",
+            inline=False,
+        )
+        await interaction.response.send_message(embed=embed)
+        await logger(
+            interaction,
+            f"TIJK Bot was restarted by {interaction.user}",
+            interaction.channel.name,
+        )
+        await self.bot.change_presence(
+            activity=nextcord.Activity(
+                type=nextcord.ActivityType.playing, name="Restarting..."
             )
-            await interaction.response.send_message(embed=embed)
-            await logger(
-                interaction,
-                f"TIJK Bot was restarted by {interaction.user}",
-                interaction.channel.name,
-            )
-            await self.bot.change_presence(
-                activity=nextcord.Activity(
-                    type=nextcord.ActivityType.playing, name="Restarting..."
-                )
-            )
-            command = "cls" if os.name in ("nt", "dos") else "clear"
-            os.system(command)
-            os.execv(sys.executable, ["python"] + sys.argv)
-        else:
-            await interaction.response.send_message(
-                "You do not have permission to perform this task", ephemeral=True
-            )
+        )
+        command = "cls" if os.name in ("nt", "dos") else "clear"
+        os.system(command)
+        os.execv(sys.executable, ["python"] + sys.argv)
 
 
 def setup(bot):
