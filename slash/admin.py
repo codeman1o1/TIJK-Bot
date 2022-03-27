b758b5be262b28a0559565f01a2584b7b7f1d892
codeman1o1 <karsten.herijgers@gmail.com>
2022-03-27 11:28:04 +0200

    Added button roles slash

---
+++ slash/admin.py
@@ -0,0 +1,171 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction, slash_command as slash
+from nextcord.application_command import SlashOption
+import nextcord.ext.application_checks as checks
+from views.button_roles import button_roles
+
+from main import interaction_logger as ilogger, SLASH_GUILDS, BOT_DATA
+
+
+class admin_slash(
+    commands.Cog, name="Admin Slash Commands", description="Slash commands for admins"
+):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.bot.add_view(button_roles(bot=self.bot))
+
+    @slash(guild_ids=SLASH_GUILDS)
+    @checks.has_any_role("Owner", "Admin", "TIJK-Bot developer")
+    async def buttonroles(self, interaction: Interaction):
+        """This will never get called since it has subcommands"""
+        pass
+
+    @buttonroles.subcommand(
+        name="send",
+        description="Sends a message with buttons where people can get roles",
+        inherit_hooks=True,
+    )
+    async def send_buttonroles(
+        self,
+        interaction: Interaction,
+        text: str = SlashOption(
+            description="Text that will be used with the message",
+            required=False,
+            default="Click a button to add/remove that role!",
+        ),
+    ):
+        buttonroles = BOT_DATA.find_one()["buttonroles"]
+        roles = sum(
+            1
+            for buttonrole in buttonroles
+            if nextcord.utils.get(interaction.guild.roles, id=buttonrole)
+        )
+        if roles > 0:
+            embed = nextcord.Embed(color=0x0DD91A, title="The message has been sent!")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            embed = nextcord.Embed(color=0x0DD91A, title=text)
+
+            await interaction.channel.send(
+                embed=embed, view=button_roles(guild=interaction.guild)
+            )
+        else:
+            embed = nextcord.Embed(
+                color=0xFFC800,
+                title="There are no button roles!\nMake sure to add them by using `.buttonroles add <role>`",
+            )
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @buttonroles.subcommand(
+        name="add", description="Add a button role", inherit_hooks=True
+    )
+    async def add_buttonroles(
+        self,
+        interaction: Interaction,
+        role: nextcord.Role = SlashOption(description="The role to add", required=True),
+    ):
+        buttonroles = BOT_DATA.find_one()["buttonroles"]
+        roles = sum(
+            1
+            for buttonrole in buttonroles
+            if nextcord.utils.get(interaction.guild.roles, id=buttonrole)
+        )
+
+        if roles >= 25:
+            embed = nextcord.Embed(
+                color=0xFFC800, title="You can only add 25 button roles!"
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        elif role.id not in buttonroles:
+            buttonroles.append(role.id)
+            BOT_DATA.update_one(
+                {},
+                {"$set": {"buttonroles": buttonroles}},
+                upsert=False,
+            )
+            embed = nextcord.Embed(
+                color=0x0DD91A,
+                title=f'The "{role.name}" role has been added!\nMake sure to use `.buttonroles` again!',
+            )
+            await ilogger(
+                interaction, f'The "{role.name}" role has been added to buttonroles'
+            )
+            await interaction.response.send_message(embed=embed)
+        else:
+            embed = nextcord.Embed(
+                color=0x0DD91A, title=f'The "{role.name}" role is already listed!'
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @buttonroles.subcommand(
+        name="remove", description="Remove a button role", inherit_hooks=True
+    )
+    async def remove_buttonroles(
+        self,
+        interaction: Interaction,
+        role: nextcord.Role = SlashOption(
+            description="The role to remove", required=True
+        ),
+    ):
+        buttonroles = BOT_DATA.find_one()["buttonroles"]
+        if role.id in buttonroles:
+            buttonroles.remove(role.id)
+            BOT_DATA.update_one(
+                {},
+                {"$set": {"buttonroles": buttonroles}},
+                upsert=False,
+            )
+            embed = nextcord.Embed(
+                color=0x0DD91A,
+                title=f'The "{role.name}" role has been removed!\nMake sure to use `.buttonroles` again!',
+            )
+            await ilogger(
+                interaction, f'The "{role.name}" role has been removed from buttonroles'
+            )
+            await interaction.response.send_message(embed=embed)
+        else:
+            embed = nextcord.Embed(
+                color=0x0DD91A, title=f'The "{role.name}" role is not listed!'
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @buttonroles.subcommand(
+        name="list", description="Lists all button roles", inherit_hooks=True
+    )
+    async def list_buttonroles(self, interaction: Interaction):
+        buttonroles = BOT_DATA.find_one()["buttonroles"]
+        roles = sum(
+            1
+            for buttonrole in buttonroles
+            if nextcord.utils.get(interaction.guild.roles, id=buttonrole)
+        )
+        if roles > 0:
+            buttonroles2 = ""
+            for buttonrole in buttonroles:
+                if nextcord.utils.get(interaction.guild.roles, id=buttonrole):
+                    buttonroles2 = (
+                        buttonroles2
+                        + "\n> "
+                        + nextcord.utils.get(
+                            interaction.guild.roles, id=buttonrole
+                        ).name
+                    )
+            embed = nextcord.Embed(color=0x0DD91A)
+            embed.add_field(
+                name="The current button roles are:",
+                value=buttonroles2,
+                inline=False,
+            )
+            embed.set_footer(text=f"{roles}/25 buttonroles")
+        else:
+            embed = nextcord.Embed(color=0xFFC800, title="There are no button roles")
+
+        await interaction.response.send_message(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(admin_slash(bot))
