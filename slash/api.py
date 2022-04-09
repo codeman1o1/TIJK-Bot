5330c9c339360422fd2599de6bafe21955187803
codeman1o1 <karsten.herijgers@gmail.com>
2022-04-09 14:12:16 +0200

    Added `api hypixel` slash command

--- slash/api.py
+++ slash/api.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+from PIL import Image
+import math
 import aiohttp
+import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction, slash_command as slash
 from nextcord.application_command import SlashOption
-from main import SLASH_GUILDS
+from mojang import MojangAPI
+import requests
+import os
+
+from main import SLASH_GUILDS, HYPIXEL_API_KEY
 
 
 class api_slash(
@@ -42,6 +50,135 @@ class api_slash(
             request = await session.get(f"https://some-random-api.ml/img/{animal}")
             info = await request.json()
         await interaction.response.send_message(info["link"])
+
+    @api.subcommand(
+        name="hypixel",
+        description="Gets information from the official Hypixel API",
+        inherit_hooks=True,
+    )
+    async def hypixel_api(
+        self,
+        interaction: Interaction,
+        username: str = SlashOption(
+            description="The username of the user", required=True
+        ),
+    ):
+        uuid = MojangAPI.get_uuid(username)
+        if not uuid:
+            embed = nextcord.Embed(color=0xFFC800, title="That is not a user!")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+        profile = MojangAPI.get_profile(uuid)
+        skin_url = profile.skin_url
+        skin = Image.open(requests.get(skin_url, stream=True).raw)
+        head_img = skin.crop((8, 8, 16, 16))
+        head_img = head_img.resize((128, 128), Image.NEAREST)
+        head_img.save("head_img.png")
+        head_img.close()
+        file = nextcord.File("head_img.png")
+        async with aiohttp.ClientSession() as session:
+            request = await session.get(
+                f"https://api.hypixel.net/player?key={HYPIXEL_API_KEY}&uuid={uuid}"
+            )
+            friends_request = await session.get(
+                f"https://api.hypixel.net/friends?key={HYPIXEL_API_KEY}&uuid={uuid}"
+            )
+            guild_request = await session.get(
+                f"https://api.hypixel.net/guild?key={HYPIXEL_API_KEY}&player={uuid}"
+            )
+            data = await request.json()
+            data_friends = await friends_request.json()
+            data_guild = await guild_request.json()
+
+        if data["success"]:
+            player_data = data["player"]
+            if "rank" in player_data:
+                rank = player_data["rank"]
+            elif "monthlyPackageRank" in player_data:
+                rank = player_data["monthlyPackageRank"].replace("_PLUS", "+")
+            elif "newPackageRank" in player_data:
+                rank = player_data["newPackageRank"].replace("_PLUS", "+")
+            else:
+                rank = ""
+            display_name = data["player"]["displayname"]
+            embed = nextcord.Embed(
+                color=0x0DD91A,
+                title=f"Here is information for **{rank} {display_name}**:",
+            )
+            embed.set_thumbnail(url="attachment://head_img.png")
+            if "lastLogout" in player_data:
+                logouttime = player_data["lastLogout"]
+                logintime = player_data["lastLogin"]
+                if logouttime < logintime:
+                    embed.add_field(
+                        name="Last online:",
+                        value=f"{display_name} is currently online",
+                        inline=False,
+                    )
+                else:
+                    offline_for = datetime.fromtimestamp(
+                        datetime.now().timestamp() - (logouttime / 1000)
+                    ).strftime("%d days and %H hours")
+                    embed.add_field(
+                        name="Last online:",
+                        value=datetime.fromtimestamp(logouttime / 1000).strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        )
+                        + "\nOffline for: "
+                        + offline_for,
+                        inline=False,
+                    )
+            else:
+                embed.add_field(
+                    name="Last online:",
+                    value="This is not enabled for this player",
+                    inline=False,
+                )
+            embed.add_field(
+                name="Friends:", value=len(data_friends["records"]), inline=False
+            )
+            if data_guild["success"] and data_guild["guild"] is not None:
+                name = data_guild["guild"]["name"]
+                members = str(len(data_guild["guild"]["members"]))
+                embed.add_field(
+                    name="Guild:",
+                    value=f"{name} ({members} members)",
+                    inline=False,
+                )
+            else:
+                embed.add_field(
+                    name="Guild:",
+                    value=f"{display_name} is not in a guild",
+                    inline=False,
+                )
+            if "socialMedia" in player_data and "links" in player_data["socialMedia"]:
+                social_media = "".join(
+                    f"{k.capitalize()}: {i}\n"
+                    for k, i in player_data["socialMedia"]["links"].items()
+                )
+                if not social_media:
+                    social_media = "This player has no Social Media linked"
+                embed.add_field(name="Social Media", value=social_media, inline=False)
+            else:
+                embed.add_field(
+                    name="Social Media",
+                    value="This player has no Social Media linked",
+                    inline=False,
+                )
+            network_experience = player_data["networkExp"]
+            network_level = (math.sqrt((2 * network_experience) + 30625) / 50) - 2.5
+            network_level = round(network_level, 2)
+            embed.add_field(name="Network Level", value=network_level, inline=False)
+            karma = player_data["karma"]
+            embed.add_field(name="Karma", value=f"{karma:,}", inline=False)
+        else:
+            error = data["cause"]
+            embed = nextcord.Embed(color=0xFF0000)
+            embed.add_field(
+                name="An error occurred!", value=f"Error provided by the API:\n{error}"
+            )
+        await interaction.response.send_message(embed=embed, file=file)
+        os.remove("head_img.png")
 
 
 def setup(bot: commands.Bot):
