e24dbbf2eb97385781515c621dea2e55842e8204
codeman1o1 <karsten.herijgers@gmail.com>
2022-04-11 19:40:48 +0200

    Added `hypixelparty`

--- slash/general.py
+++ slash/general.py
@@ -1,10 +1,13 @@
+import random
 import nextcord
 from nextcord import Interaction, slash_command as slash
 from nextcord.ext import commands
+import requests
+from mojang import MojangAPI
 
 from views.buttons.github import github_button
 
-from main import SLASH_GUILDS
+from main import HYPIXEL_API_KEY, SLASH_GUILDS, USER_DATA
 
 
 class general_slash(
@@ -27,6 +30,53 @@ class general_slash(
             inline=False,
         )
         await interaction.response.send_message(embed=embed, view=github_button())
+
+    @slash(description="Chooses a random player who can own the party", guild_ids=SLASH_GUILDS)
+    async def hypixelparty(self, interaction: Interaction):
+        hypixel_ping = nextcord.utils.get(interaction.guild.roles, name="Hypixel Ping")
+        available = [
+            user
+            for user in interaction.channel.members
+            if not user.bot
+            if user.status != nextcord.Status.offline
+            if hypixel_ping in user.roles
+        ]
+        if available:
+            for user in available:
+                user2 = await commands.converter.UserConverter().convert(
+                    interaction, str(user)
+                )
+                query = {"_id": user2.id}
+                if USER_DATA.count_documents(query) != 0:
+                    user3 = USER_DATA.find_one(query)
+                    if "minecraft_account" in user3:
+                        username = user3["minecraft_account"]
+                        uuid = MojangAPI.get_uuid(username)
+                        data = requests.get(
+                            f"https://api.hypixel.net/player?key={HYPIXEL_API_KEY}&uuid={uuid}"
+                        ).json()
+                        logouttime = data["player"]["lastLogout"]
+                        logintime = data["player"]["lastLogin"]
+                        if logouttime >= logintime or data["success"] is False:
+                            available.remove(user)
+                    else:
+                        available.remove(user)
+                else:
+                    available.remove(user)
+        if available:
+            embed = nextcord.Embed(color=0x0DD91A)
+            embed.add_field(
+                name="Party leader chosen!",
+                value=f"{random.choice(available)} will be the party leader!",
+                inline=False,
+            )
+        else:
+            embed = nextcord.Embed(
+                color=0x0DD91A,
+                title="Nobody meets the requirements to be the party leader!",
+            )
+        await interaction.response.send_message(embed=embed, delete_after=300)
+        del available
 
 
 def setup(bot: commands.Bot):
