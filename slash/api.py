b6701ca2b5e1447dc0bf53ca53843f1247db8591
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2022-06-06 15:21:54 +0200

    Removed `/api hypixel`

--- slash/api.py
+++ slash/api.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-import math
 import os
 from PIL import Image
 import aiohttp
@@ -10,7 +8,7 @@ from nextcord.application_command import SlashOption
 from mojang import MojangAPI
 import requests
 
-from main import SLASH_GUILDS, HYPIXEL_API_KEY
+from main import SLASH_GUILDS
 
 
 async def embed_with_mc_head(
@@ -67,123 +65,6 @@ class Api(commands.Cog):
             request = await session.get(f"https://some-random-api.ml/img/{animal}")
             info = await request.json()
         await interaction.response.send_message(info["link"])
-
-    @api.subcommand(name="hypixel", inherit_hooks=True)
-    async def hypixel_api(
-        self,
-        interaction: Interaction,
-        username: str = SlashOption(
-            description="The username of the user", required=True
-        ),
-    ):
-        """Gets information from the official Hypixel API"""
-        uuid = MojangAPI.get_uuid(username)
-        if not uuid:
-            embed = nextcord.Embed(color=0xFFC800, title="That is not a user!")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            return
-        async with aiohttp.ClientSession() as session:
-            request = await session.get(
-                f"https://api.hypixel.net/player?key={HYPIXEL_API_KEY}&uuid={uuid}"
-            )
-            friends_request = await session.get(
-                f"https://api.hypixel.net/friends?key={HYPIXEL_API_KEY}&uuid={uuid}"
-            )
-            guild_request = await session.get(
-                f"https://api.hypixel.net/guild?key={HYPIXEL_API_KEY}&player={uuid}"
-            )
-            data = await request.json()
-            data_friends = await friends_request.json()
-            data_guild = await guild_request.json()
-
-        if data["success"]:
-            player_data = data["player"]
-            if "rank" in player_data:
-                rank = player_data["rank"]
-            elif "monthlyPackageRank" in player_data:
-                rank = player_data["monthlyPackageRank"].replace("_PLUS", "+")
-            elif "newPackageRank" in player_data:
-                rank = player_data["newPackageRank"].replace("_PLUS", "+")
-            else:
-                rank = ""
-            display_name = data["player"]["displayname"]
-            embed = nextcord.Embed(
-                color=0x0DD91A,
-                title=f"Here is information for **{rank} {display_name}**:",
-            )
-            if "lastLogout" in player_data:
-                logouttime = player_data["lastLogout"]
-                logintime = player_data["lastLogin"]
-                if logouttime < logintime:
-                    embed.add_field(
-                        name="Last online:",
-                        value=f"{display_name} is currently online",
-                        inline=False,
-                    )
-                else:
-                    offline_for = datetime.fromtimestamp(
-                        datetime.now().timestamp() - (logouttime / 1000)
-                    ).strftime("%d days and %H hours")
-                    embed.add_field(
-                        name="Last online:",
-                        value=datetime.fromtimestamp(logouttime / 1000).strftime(
-                            "%Y-%m-%d %H:%M:%S"
-                        )
-                        + "\nOffline for: "
-                        + offline_for,
-                        inline=False,
-                    )
-            else:
-                embed.add_field(
-                    name="Last online:",
-                    value="This is not enabled for this player",
-                    inline=False,
-                )
-            embed.add_field(
-                name="Friends:", value=len(data_friends["records"]), inline=False
-            )
-            if data_guild["success"] and data_guild["guild"] is not None:
-                name = data_guild["guild"]["name"]
-                members = str(len(data_guild["guild"]["members"]))
-                embed.add_field(
-                    name="Guild:",
-                    value=f"{name} ({members} members)",
-                    inline=False,
-                )
-            else:
-                embed.add_field(
-                    name="Guild:",
-                    value=f"{display_name} is not in a guild",
-                    inline=False,
-                )
-            if "socialMedia" in player_data and "links" in player_data["socialMedia"]:
-                social_media = "".join(
-                    f"{k.capitalize()}: {i}\n"
-                    for k, i in player_data["socialMedia"]["links"].items()
-                )
-                if not social_media:
-                    social_media = "This player has no Social Media linked"
-                embed.add_field(name="Social Media", value=social_media, inline=False)
-            else:
-                embed.add_field(
-                    name="Social Media",
-                    value="This player has no Social Media linked",
-                    inline=False,
-                )
-            network_experience = player_data["networkExp"]
-            network_level = (math.sqrt((2 * network_experience) + 30625) / 50) - 2.5
-            network_level = round(network_level, 2)
-            embed.add_field(name="Network Level", value=network_level, inline=False)
-            karma = player_data["karma"]
-            embed.add_field(name="Karma", value=f"{karma:,}", inline=False)
-            await embed_with_mc_head(interaction, embed, username)
-        else:
-            error = data["cause"]
-            embed = nextcord.Embed(color=0xFF0000)
-            embed.add_field(
-                name="An error occurred!", value=f"Error provided by the API:\n{error}"
-            )
-            await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @api.subcommand(name="minecraft", inherit_hooks=True)
     async def minecraft_api(
