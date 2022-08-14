9320452b2b9a087a8bec25cb80b1f3ed52bde030
JustIanJ <90295730+JustIanJ@users.noreply.github.com>
2022-08-14 20:16:03 +0200

    edit in text
    The previous "everyone" suggests they are all human, but this command also sends the amounts of bots, so this is a more neutral "each user"

--- slash/fun.py
+++ slash/fun.py
@@ -71,7 +71,7 @@ class Fun(commands.Cog):
 
     @slash(guild_ids=SLASH_GUILDS)
     async def messages(self, interaction: Interaction):
-        """Show the amount of messages everyone has sent"""
+        """Show the amount of messages each user has sent"""
         embed = nextcord.Embed(color=0x0DD91A)
         for user in USER_DATA.find().sort("messages", pymongo.DESCENDING):
             if "messages" in user:
